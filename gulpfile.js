a473c7cb34c83043829a98906cff62f0c6676caa
Anthony Ramella <alramella@gmail.com>
2016-02-23 16:33:18 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+/**
+ * This example:
+ *  Uses the built-in BrowserSync server for HTML files
+ *  Watches & compiles SASS files
+ *  Watches & injects CSS files
+ *
+ * More details: http://www.browsersync.io/docs/gulp/
+ *
+ * Install:
+ * npm install browser-sync gulp gulp-sass --save-dev
+ *
+ * Then run it with:
+ * gulp
+ */
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+
+// Browser-sync task, only cares about compiled CSS
+gulp.task('browser-sync', function () {
+    browserSync({
+        proxy: "http://internalwiki.dev/"
+    });
+});
+
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(reload({stream:true}));
+});
+
+// Reload all Browsers
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+// Default task to be run with `gulp`
+// This default task will run BrowserSync & then use Gulp to watch files.
+// When a file is changed, an event is emitted to BrowserSync with the filepath.
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch('css/*.css', function (file) {
+        if (file.type === "changed") {
+            reload(file.path);
+        }
+    });
+    gulp.watch("themes/user/wiki_themes/foundation/*.html", ['bs-reload']);
+});
